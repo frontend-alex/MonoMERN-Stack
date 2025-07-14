4480390709907bbd0887140375fd7c8984995d6d
Aleksander Ivanov <62203063+frontend-alex@users.noreply.github.com>
2025-07-14 20:38:04 +0200

    changes

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -2,23 +2,23 @@ import AppLogo from "./AppLogo";
 import { lazy, Suspense } from "react";
 import { Link } from "react-router-dom";
 import { UserDropdownSkeleton } from "./dropdowns/user-dropdown";
+import { useAuth } from "@/contexts/AuthContext";
+import { Button } from "./ui/button";
 
-const LazyUserDropdown = lazy(  
+const LazyUserDropdown = lazy(
   () => import("@/components/dropdowns/user-dropdown")
 );
 
 const navLinks = [
   {
-    name: "Home",
-    path: "/",
-  },
-  {
-    name: "Docs",
-    path: "/docs",
+    name: "",
+    path: "",
   },
 ];
 
 const Navbar = () => {
+  const { isAuthenticated } = useAuth();
+
   return (
     <nav className="flex justify-between max-w-7xl mx-auto items-center gap-3 p-5">
       <AppLogo />
@@ -31,9 +31,20 @@ const Navbar = () => {
           </li>
         ))}
       </ul>
-      <Suspense fallback={<UserDropdownSkeleton />}>
-        <LazyUserDropdown />
-      </Suspense>
+      {isAuthenticated ? (
+        <Suspense fallback={<UserDropdownSkeleton />}>
+          <LazyUserDropdown />
+        </Suspense>
+      ) : (
+        <div className="flex items-center gap-3">
+          <Link to="/login">
+            <Button variant={'ghost'}>Log in</Button>
+          </Link>
+          <Link to="/register">
+            <Button>Create an account</Button>
+          </Link>
+        </div>
+      )}
     </nav>
   );
 };
